10178c7afc59965e60ae21c1171e30a4c2337510
JarekIT <JaroslawDanielewiczIT@gmail.com>
2021-04-14 18:55:42 +0200

    create GET method for cities

--- backend/src/cities/controllers/cities.controller.ts
+++ backend/src/cities/controllers/cities.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CitiesService } from '../services/cities.service';
 
 @Controller('cities')
-export class CitiesController {}
+export class CitiesController {
+  constructor(public readonly citiesService: CitiesService) {}
+
+  @Get()
+  async getAll() {
+    this.citiesService.getAll();
+  }
+}
