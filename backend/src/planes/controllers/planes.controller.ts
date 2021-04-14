bd8ad25c099a7e93ba990abf9f89c6925b694dab
JarekIT <JaroslawDanielewiczIT@gmail.com>
2021-04-14 20:10:40 +0200

    create GET methods for planes

--- backend/src/planes/controllers/planes.controller.ts
+++ backend/src/planes/controllers/planes.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { PlanesService } from '../services/planes.service';
+import { ICargoPlane, IEnemyPlane } from 'src/types/types';
 
 @Controller('planes')
-export class PlanesController {}
+export class PlanesController {
+  constructor(private readonly planesService: PlanesService) {}
+
+  @Get('enemies')
+  async getAllEnemies(): Promise<IEnemyPlane[]> {
+    return await this.planesService.getAllEnemies();
+  }
+
+  @Get('cargos')
+  async getAllCargos(): Promise<ICargoPlane[]> {
+    return await this.planesService.getAllCargos();
+  }
+}
