c462c2e61c7fe2d60bafe3127cf049cf7e9e13ab
JarekIT <JaroslawDanielewiczIT@gmail.com>
2021-04-14 20:21:21 +0200

    create getAllEnemies and getAllCargos in planes service

--- backend/src/planes/services/planes.service.ts
+++ backend/src/planes/services/planes.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { ICargoPlane, IEnemyPlane } from 'src/types/types';
 
 @Injectable()
-export class PlanesService {}
+export class PlanesService {
+  async getAllEnemies(): Promise<IEnemyPlane[]> {
+    let enemies: IEnemyPlane[] = [];
+
+    fetch('../../db/enemies.json')
+      .then((response) => response.json())
+      .then((data: IEnemyPlane[]) => (enemies = data))
+      .catch((err) => console.log(err));
+
+    return enemies;
+  }
+
+  async getAllCargos(): Promise<ICargoPlane[]> {
+    let cargos: ICargoPlane[] = [];
+
+    fetch('../../db/cargos.json')
+      .then((response) => response.json())
+      .then((data: ICargoPlane[]) => (cargos = data))
+      .catch((err) => console.log(err));
+
+    return cargos;
+  }
+}
