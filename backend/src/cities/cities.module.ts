71d42bce11db457da74ee8ba1c61b8d24f4ef05e
JarekIT <JaroslawDanielewiczIT@gmail.com>
2021-04-14 19:24:20 +0200

    correct typos

--- backend/src/cities/cities.module.ts
+++ backend/src/cities/cities.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { CitiesService } from './cities.service';
-import { CitiesController } from './cities.controller';
+import { CitiesService } from './services/cities.service';
+import { CitiesController } from './controllers/cities.controller';
 
 @Module({
   providers: [CitiesService],
--- backend/src/cities/controllers/cities.controller.spec.ts
+++ backend/src/cities/controllers/cities.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CitiesService } from '../services/cities.service';
 import { CitiesController } from './cities.controller';
 
 describe('CitiesController', () => {
@@ -7,6 +8,7 @@ describe('CitiesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CitiesController],
+      providers: [CitiesService],
     }).compile();
 
     controller = module.get<CitiesController>(CitiesController);
