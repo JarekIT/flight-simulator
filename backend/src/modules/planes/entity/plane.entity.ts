4c0bbfa519e5129392501e27a7738400073b3229
JarekIT <JaroslawDanielewiczIT@gmail.com>
2021-04-18 14:36:18 +0200

    Create plane.entity.ts

---
+++ backend/src/modules/planes/entity/plane.entity.ts
@@ -0,0 +1,55 @@
+import { ILocation, IPlane } from 'src/types/all.types';
+
+export class Plane implements IPlane {
+  constructor(public uuid: string, public name: string, public speed: number) {}
+
+  flightAngle?: number;
+  location: ILocation;
+
+  move() {
+    const distanceInKm = this.convertSpeedToDistanceInOneInterval(this.speed);
+    const distanceInLatLng: number = distanceInKm / 111;
+
+    // if defined - chose path (cargo) // if undefined = random direction (enemy)
+    const directionInDegrees: number =
+      this.flightAngle || this.getRandomDirection();
+
+    const deltaLat =
+      Math.cos(directionInDegrees * (Math.PI / 180)) * distanceInLatLng;
+    const deltaLng =
+      Math.sin(directionInDegrees * (Math.PI / 180)) * distanceInLatLng;
+
+    const newLat = this.location.lat + deltaLat;
+    const newLng = this.location.lng + deltaLng;
+
+    console.log(
+      `Move: ${this.name} | dist: ${distanceInKm.toFixed(
+        1,
+      )}km | angle: ${directionInDegrees.toFixed(
+        0,
+      )} | loc: { lat:${newLat.toFixed(3)} , lng: ${newLng.toFixed(
+        3,
+      )}} | change: { ${deltaLat.toFixed(4)}, ${deltaLng.toFixed(4)} }`,
+    );
+
+    this.setNewLocation(newLat, newLng);
+  }
+
+  private getRandomDirection(): number {
+    return Math.floor(Math.random() * 361);
+  }
+
+  private setNewLocation(lat: number, lng: number): void {
+    this.location = {
+      lat: lat,
+      lng: lng,
+    };
+  }
+
+  convertSpeedToDistanceInOneInterval(speed: number): number {
+    // if we assume, that 1h in real = 10 sec in simulator
+    // so 100 km/h => 10 km in 1 sec
+    const scale = 0.1;
+    return scale * speed;
+  }
+}
