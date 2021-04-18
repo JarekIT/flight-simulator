2ca0a57633a7160e91ca3c367e982639b8848d51
JarekIT <JaroslawDanielewiczIT@gmail.com>
2021-04-18 16:19:42 +0200

    update app.service (project initialization, setting the interval)

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { IntervalService } from './shared-services/interval.service';
+import { ManagerService } from './shared-services/manager.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly intervalService: IntervalService,
+    private readonly managerService: ManagerService,
+  ) {
+    this.prepareCargos(10);
+    this.prepareEnemies(10);
+    this.initInterval(1000);
+  }
+
+  private initInterval(movePlaneIntervalInMs: number): void {
+    this.intervalService.startInterval(movePlaneIntervalInMs);
+  }
+
+  private prepareCargos(nrOfPlanes: number) {
+    for (let i = 0; i < nrOfPlanes; i++) {
+      this.managerService.createCargo();
+    }
+  }
+
+  private prepareEnemies(nrOfPlanes: number) {
+    for (let i = 0; i < nrOfPlanes; i++) {
+      this.managerService.createEnemy();
+    }
   }
 }
