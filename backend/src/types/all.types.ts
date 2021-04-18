db323f6fa8b4aba55dcc62a6228c443ff728e46b
JarekIT <JaroslawDanielewiczIT@gmail.com>
2021-04-18 14:11:09 +0200

    update Types (clean unused code, add enums)

---
+++ backend/src/types/all.types.ts
@@ -0,0 +1,55 @@
+import { Cargo } from '../modules/planes/entity/cargo.entity';
+import { Enemy } from '../modules/planes/entity/enemy.entity';
+
+export interface ILocation {
+  lat: number;
+  lng: number;
+}
+
+interface IIdentity {
+  uuid: string;
+  name: string;
+  location: ILocation;
+}
+
+export interface ICity extends IIdentity {
+  shortName: string;
+}
+
+export interface IPlane extends IIdentity {
+  location: ILocation;
+  speed: number;
+  flightAngle?: number;
+  move: () => void;
+}
+
+export interface IEnemyPlane extends IPlane {
+  fireRange: number;
+  inRange: (c: Cargo) => boolean;
+  shoot: (c: Cargo) => void;
+}
+
+export interface ICargoPlane extends IPlane {
+  alive: boolean;
+  start: ICity;
+  end: ICity;
+  landed: boolean;
+  hitBy?: string;
+  flyAway: () => void;
+  board: () => void;
+}
+
+export type PlaneType = Cargo | Enemy;
+
+export enum Planes {
+  ENEMY,
+  CARGO,
+}
+
+export interface IFactory {
+  create: (plane: Planes) => PlaneType;
+}
+
+export interface ISpawner {
+  spawn: (plane: PlaneType) => void;
+}
