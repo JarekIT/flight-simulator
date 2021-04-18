47d78c2bae659ad2439491dac461e9b84b21c9e6
JarekIT <JaroslawDanielewiczIT@gmail.com>
2021-04-18 16:17:19 +0200

    Update app.module (provide all needed Services)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,37 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CitiesModule } from './modules/cities/cities.module';
+import { CitiesController } from './modules/cities/controllers/cities.controller';
+import { CitiesService } from './modules/cities/services/cities.service';
+import { PlanesController } from './modules/planes/controllers/planes.controller';
 import { PlanesModule } from './modules/planes/planes.module';
+import { CargoService } from './modules/planes/services/cargo.service';
+import { EnemyService } from './modules/planes/services/enemy.service';
+import { DispatcherService } from './shared-services/dispatcher.service';
+import { CargoFactoryService } from './shared-services/factories/cargo-factory.service';
+import { EnemyFactoryService } from './shared-services/factories/enemy-factory.service';
+import { FactoryService } from './shared-services/factory.service';
+import { IntervalService } from './shared-services/interval.service';
+import { ManagerService } from './shared-services/manager.service';
+import { MoverService } from './shared-services/mover.service';
+import { SpawnService } from './shared-services/spawn.service';
 
 @Module({
   imports: [CitiesModule, PlanesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [CitiesController, PlanesController],
+  providers: [
+    AppService,
+    IntervalService,
+    MoverService,
+    FactoryService,
+    CitiesService,
+    EnemyService,
+    CargoService,
+    SpawnService,
+    CargoFactoryService,
+    EnemyFactoryService,
+    ManagerService,
+    DispatcherService,
+  ],
 })
 export class AppModule {}
